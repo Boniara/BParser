2285d6ca3d239dbd2b02f4e4176aef455dc2239d
boniara <rutkovba@gmail.com>
2016-07-18 11:12:15 +0300

    Add DBConnectionConstantSource.java

---
+++ src/main/java/com/bonia/BParser/utils/jdbc/DBConnectionConstantSource.java
@@ -0,0 +1,9 @@
+package com.bonia.BParser.utils.jdbc;
+
+public interface DBConnectionConstantSource {
+
+    String DRIVER_NAME = "com.mysql.jdbc.Driver";
+    String URL = "jdbc:mysql://localhost:3306/company";
+    String USERNAME = "root";
+    String PASSWORD = "root";
+}
